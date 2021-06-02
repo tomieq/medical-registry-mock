53a067b7ae80b9c33aef108dee435420b94dc99d
tomieq <tomieq@wp.pl>
2021-06-02 14:54:40 +0200

    Created stub for WebApplication

---
+++ Sources/webApp/WebApplication.swift
@@ -0,0 +1,15 @@
+//
+//  WebApplication.swift
+//  
+//
+//  Created by Tomasz Kucharski on 12/03/2021.
+//
+
+import Foundation
+import Swifter
+
+class WebApplication {
+    
+    init(_ server: HttpServer) {
+    }
+}
